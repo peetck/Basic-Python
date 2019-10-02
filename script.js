4a41be4f5b04cf3bbe9fd83f92a4acc0e127f444
Sirawit yodjit <42561981+blackjokerrr@users.noreply.github.com>
2019-10-02 18:58:09 +0700

    UPPPPer

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ window.addEventListener("wheel", function(x){
         document.getElementById("Header").style.color = 'hsl(211, 0%, 100%)';
     }
     if(Temp >= 1 && Temp < 6){
-        document.getElementById("Header").style.color = 'hsl(211, 100%, '+(100-(Temp*10)+'%)');
+        document.getElementById("Header").style.color = 'hsl(211, 100%, '+(100-(Temp*10))+'%)';
         document.getElementById("why").style.color = 'hsl(211, 100%, 50%)';
-        document.getElementById("temp").style.color = 'hsl(211, 100%, 50%)';
-    }if(Temp >= 6){
-        document.getElementById("why").style.color = 'hsl(211, 0%, 100%)';
-        document.getElementById("temp").style.color = 'hsl(211, 0%, 100%)';
+        document.getElementById("temp_why").style.color = 'hsl(211, 100%, 50%)';
+        document.getElementById("like").style.color = 'hsl(211, 100%, 50%)';
+        document.getElementById("temp_like").style.color = 'hsl(211, 100%, 50%)';
+    }if(Temp >= 6 && Temp <= 11){
+        document.getElementById("why").style.color = 'hsl(211, '+(Number(Temp!=6)*100)+'%, '+(100-((Temp-6)*10))+'%)';
+        document.getElementById("temp_why").style.color = 'hsl(211, '+(Number(Temp!=6)*100)+'%, '+(100-((Temp-6)*10))+'%)';
+        document.getElementById("like").style.color = 'hsl(211, 100%, 50%)';
+        document.getElementById("temp_like").style.color = 'hsl(211, 100%, 50%)';
+    }if(Temp >= 11){
+        document.getElementById("like").style.color = 'hsl(211, '+(Number(Temp!=11)*100)+'%, '+(100-((Temp-11)*10))+'%)';
+        document.getElementById("temp_like").style.color = 'hsl(211, '+(Number(Temp!=11)*100)+'%, '+(100-((Temp-11)*10))+'%)';
     }
-    console.log(Temp);
 });
