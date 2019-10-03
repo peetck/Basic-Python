7a783455984b079f69c4739488bb4d36ba9a7a73
Sirawit yodjit <42561981+blackjokerrr@users.noreply.github.com>
2019-10-03 20:49:17 +0700

    UPUPUPU

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 var Temp = 0;
 window.addEventListener("wheel", function(x){
     Temp += Math.sign(x.deltaY);
-    if(Temp <= 0){
-        Temp = 0; 
-        document.getElementById("Header").style.color = 'hsl(211, 0%, 100%)';
+    if(Temp < 18){
+        if(Temp <= 0){
+            Temp = 0; 
+            document.getElementById("Header").style.color = 'hsl(211, 0%, 100%)';
+        }
+        if(Temp >= 1 && Temp < 6){
+            document.getElementById("Header").style.color = 'hsl(211, 100%, '+(100-(Temp*10))+'%)';
+            document.getElementById("why").style.color = 'hsl(211, 100%, 50%)';
+            document.getElementById("temp_why").style.color = 'hsl(211, 100%, 50%)';
+            document.getElementById("like").style.color = 'hsl(211, 100%, 50%)';
+            document.getElementById("temp_like").style.color = 'hsl(211, 100%, 50%)';
+            document.getElementById("Invite").style.color = 'hsl(211, 100%, 50%)';
+        }if(Temp >= 6 && Temp <= 11){
+            document.getElementById("why").style.color = 'hsl(211, '+(Number(Temp!=6)*100)+'%, '+(100-((Temp-6)*10))+'%)';
+            document.getElementById("temp_why").style.color = 'hsl(211, '+(Number(Temp!=6)*100)+'%, '+(100-((Temp-6)*10))+'%)';
+            document.getElementById("like").style.color = 'hsl(211, 100%, 50%)';
+            document.getElementById("temp_like").style.color = 'hsl(211, 100%, 50%)';
+            document.getElementById("Invite").style.color = 'hsl(211, 100%, 50%)';
+        }if(Temp >= 11 && Temp < 16){
+            document.getElementById("like").style.color = 'hsl(211, '+(Number(Temp!=11)*100)+'%, '+(100-((Temp-11)*10))+'%)';
+            document.getElementById("temp_like").style.color = 'hsl(211, '+(Number(Temp!=11)*100)+'%, '+(100-((Temp-11)*10))+'%)';
+            document.getElementById("Invite").style.color = 'hsl(211, 100%, 50%)';
+        }if(Temp >= 16){
+            document.getElementById("Invite").style.color = 'hsl(211, '+(Number(Temp!=17)*100)+'%, '+(100-((Temp-17)*10))+'%)';
+        }
+    }else{
+        Temp = 17;
     }
-    if(Temp >= 1 && Temp < 6){
-        document.getElementById("Header").style.color = 'hsl(211, 100%, '+(100-(Temp*10))+'%)';
-        document.getElementById("why").style.color = 'hsl(211, 100%, 50%)';
-        document.getElementById("temp_why").style.color = 'hsl(211, 100%, 50%)';
-        document.getElementById("like").style.color = 'hsl(211, 100%, 50%)';
-        document.getElementById("temp_like").style.color = 'hsl(211, 100%, 50%)';
-    }if(Temp >= 6 && Temp <= 11){
-        document.getElementById("why").style.color = 'hsl(211, '+(Number(Temp!=6)*100)+'%, '+(100-((Temp-6)*10))+'%)';
-        document.getElementById("temp_why").style.color = 'hsl(211, '+(Number(Temp!=6)*100)+'%, '+(100-((Temp-6)*10))+'%)';
-        document.getElementById("like").style.color = 'hsl(211, 100%, 50%)';
-        document.getElementById("temp_like").style.color = 'hsl(211, 100%, 50%)';
-    }if(Temp >= 11){
-        document.getElementById("like").style.color = 'hsl(211, '+(Number(Temp!=11)*100)+'%, '+(100-((Temp-11)*10))+'%)';
-        document.getElementById("temp_like").style.color = 'hsl(211, '+(Number(Temp!=11)*100)+'%, '+(100-((Temp-11)*10))+'%)';
-    }
+    console.log(Temp);
 });
