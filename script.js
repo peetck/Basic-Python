0cd72e2b5daf66a2c520d841aebb35a2478f7c09
Sirawit yodjit <42561981+blackjokerrr@users.noreply.github.com>
2019-10-04 19:21:23 +0700

    UPUPU

--- script.js
+++ script.js
@@ -6,28 +6,45 @@ window.addEventListener("wheel", function(value){
 
 window.addEventListener("scroll", function(){
     let scroll = document.documentElement.scrollTop;
+    let why, temp_why, like, temp_like, invite;
+    why = document.getElementById("why");
+    temp_why = document.getElementById("temp_why");
+    like = document.getElementById("like");
+    temp_like = document.getElementById("temp_like");
+    invite = document.getElementById("Invite");
         if(scroll == 0){
             document.getElementById("Header").style.color = 'hsl(211, 0%, 100%)';
         }
         if(document.documentElement.scrollTop >= 1 && document.documentElement.scrollTop < 500){
+            why.style.display = 'none';
+            temp_why.style.display = 'none';
             document.getElementById("Header").style.color = 'hsl(211, 100%, '+(100-(scroll/10))+'%)';
-            document.getElementById("why").style.color = 'hsl(211, 100%, 50%)';
-            document.getElementById("temp_why").style.color = 'hsl(211, 100%, 50%)';
-            document.getElementById("like").style.color = 'hsl(211, 100%, 50%)';
-            document.getElementById("temp_like").style.color = 'hsl(211, 100%, 50%)';
-            document.getElementById("Invite").style.color = 'hsl(211, 100%, 50%)';
+            why.style.color = 'hsl(211, 100%, 50%)';
+            temp_why.style.color = 'hsl(211, 100%, 50%)';
+            like.style.color = 'hsl(211, 100%, 50%)';
+            temp_like.style.color = 'hsl(211, 100%, 50%)';
+            invite.style.color = 'hsl(211, 100%, 50%)';
         }if(document.documentElement.scrollTop >= 500 && document.documentElement.scrollTop < 1000){
-            document.getElementById("why").style.color = 'hsl(211, '+(Number(Temp!=500)*100)+'%, '+(100-(((scroll/100)-5)*10))+'%)';
-            document.getElementById("temp_why").style.color = 'hsl(211, '+(Number(Temp!=500)*100)+'%, '+(100-(((scroll/100)-5)*10))+'%)';
-            document.getElementById("like").style.color = 'hsl(211, 100%, 50%)';
-            document.getElementById("temp_like").style.color = 'hsl(211, 100%, 50%)';
-            document.getElementById("Invite").style.color = 'hsl(211, 100%, 50%)';
-        }if(document.documentElement.scrollTop >= 1000 && document.documentElement.scrollTop < 1582){
-            document.getElementById("like").style.color = 'hsl(211, '+(Number(Temp!=11)*100)+'%, '+(100-(((scroll/100)-11)*10))+'%)';
-            document.getElementById("temp_like").style.color = 'hsl(211, '+(Number(Temp!=11)*100)+'%, '+(100-(((scroll/100)-11)*10))+'%)';
-            document.getElementById("Invite").style.color = 'hsl(211, 100%, 50%)';
-        }if(document.documentElement.scrollTop == 1582){
-            document.getElementById("Invite").style.color = 'hsl(211, '+(Number(Temp!=17)*100)+'%, '+(100-((Math.floor(scroll/100)-15)*10))+'%)';
+            why.style.display = '';
+            temp_why.style.display = '';
+            why.style.color = 'hsl(211, '+(Number(scroll!=500)*100)+'%, '+(100-(((scroll/100)-5)*10))+'%)';
+            temp_why.style.color = 'hsl(211, '+(Number(scroll!=500)*100)+'%, '+(100-(((scroll/100)-5)*10))+'%)';
+            like.style.color = 'hsl(211, 100%, 50%)';
+            temp_like.style.color = 'hsl(211, 100%, 50%)';
+            invite.style.color = 'hsl(211, 100%, 50%)';
+            like.style.display = 'none';
+            temp_like.style.display = 'none';
+        }if(document.documentElement.scrollTop >= 1000 && document.documentElement.scrollTop < 1600){
+            like.style.display = '';
+            temp_like.style.display = '';
+            like.style.color = 'hsl(211, '+(Number(scroll!=1100)*100)+'%, '+(100-(((scroll/100)-11)*10))+'%)';
+            temp_like.style.color = 'hsl(211, '+(Number(scroll!=1100)*100)+'%, '+(100-(((scroll/100)-11)*10))+'%)';
+            invite.style.color = 'hsl(211, 100%, 50%)';
+        }if(document.documentElement.scrollTop >= 1648){
+            invite.style.display = '';
+            invite.style.color = 'hsl(211, '+(Number(scroll!=1676)*100)+'%, '+(100-((Math.floor(scroll/100)-16)*10))+'%)';
         }
     console.log(scroll);
 });
+
+console.log(why);
