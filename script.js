a152d9215a7c628bc983b3509a06c6852278d635
peetck <peekung602@gmail.com>
2019-10-09 19:46:19 +0700

    Update icon :up:

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function show(json) {
     let content = document.getElementById("content");
     let title_msg = json.title;
     let title = document.getElementById("title");
-    title.innerHTML = title_msg;
+    title.innerHTML = title_msg + "&nbsp;&nbsp;&nbsp;" + "<img class='img-fluid' width='100' src='images/" + json.logo + "'>";
     title.style.backgroundColor = json.color;
     content.innerHTML = "";
     for (let i = 0; i < json.content.length; i++) {
